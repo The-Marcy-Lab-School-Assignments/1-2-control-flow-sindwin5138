d2264b4d8263e75ce9c3ba255c0c7b825bac41dc
Cindy Nguyen <cnguyen5138@gmail.com>
2024-04-03 11:02:04 -0400

    question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,4 +1,15 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought')
+  } else if (inches > 0 && inches < 2) {
+    console.log('dry')
+  } else if (inches < 4) {
+    console.log('average')
+  } else if (inches < 6) {
+    console.log('rainy')
+  } else {
+    console.log('flood')
+  }
 };
 
 const happyBirthdayPet = () => {
--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,18 @@
 // const {
 //   measureRain,
+const measureRain = (inches) => {
+    if (inches === 0) {
+        console.log('drought')
+    } else if (inches > 0 && inches < 2) {
+        console.log('dry')
+    } else if (inches >= 2 && inches < 4) {
+        console.log('average')
+    } else if (inches >= 4 && inches < 6) {
+        console.log('rainy')
+    } else {
+        console.log('flood')
+    }
+};
 //   happyBirthdayPet,
 //   funTypes,
 //   rounder,
