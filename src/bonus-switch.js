4a2b040a2b446ec179cae7cc32738d666df57996
Cindy Nguyen <cnguyen5138@gmail.com>
2024-04-04 19:55:31 -0400

    first switch case

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,5 +1,23 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inches) => {
+  switch (true) {
+    case (inches === 0):
+      console.log("drought");
+      break;
+    case (inches > 0 && inches < 2):
+      console.log("dry");
+      break;
+    case (inches < 4):
+      console.log("average");
+      break;
+    case (inches < 6):
+      console.log("rainy");
+      break;
+    default:
+      console.log("flood")
+  }
 };
+//measureRainSwitch(0)
+//measureRainSwitch(3)
 
 const rounderSwitch = () => {
 };
