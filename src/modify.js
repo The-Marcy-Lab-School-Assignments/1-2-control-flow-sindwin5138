e886161354280ae3f2764a08b24f2297c94f5bc4
Cindy Nguyen <cnguyen5138@gmail.com>
2024-04-04 14:27:43 -0400

    mmodify

--- src/modify.js
+++ src/modify.js
@@ -1,13 +1,18 @@
 const wildlyBiasedReview = (location) => {
-  if ('NYC') {
+  if (typeof location !== 'string') {
+    return null
+  }
+  if (location === 'NYC') {
     console.log('THE GREATEST CITY IN THE WORLD');
-    console.log('THE CITY THAT NEVER SLEEPS');
-    console.log('WOW WHAT A CITY');
+    console.log('THE CITY THAT NEVER SLEEPS')
+    console.log('WOW WHAT A CITY')
   } else {
     console.log('Yea that place is cool I guess');
   }
 };
+wildlyBiasedReview('NYC')
 wildlyBiasedReview
 module.exports = {
   wildlyBiasedReview,
 };
+wildlyBiasedReview('LA')
